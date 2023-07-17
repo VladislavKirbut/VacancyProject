8a3fdd2cf0242b12612f69e3317bec726e301f13
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-07-17 12:52:19 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/vacancyproject/VacancyProjectApp.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.vacancyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyProjectApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyProjectApp.class, args);
+    }
+
+}
---
+++ src/test/java/by/teachmeskills/vacancyproject/VacancyProjectAppTests.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.vacancyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyProjectAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
